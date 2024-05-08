e7c0961e4d6c87cb14d6be66d9ca7a81417d1d37
Miguel070777 <160361356+Miguel070777@users.noreply.github.com>
2024-05-08 17:57:30 -0500

    ejercicio6.py

--- estructurasSelectivas/ejercicio6.py
+++ estructurasSelectivas/ejercicio6.py
@@ -1,14 +1,14 @@
 # Solicitamos al usuario ingresar el promedio del alumno y el costo de la pensión
-promedio_Alumno = float(input("Ingrese el promedio del alumno: "))
-costo_Pension = float(input("Ingrese el costo de la pensión: "))
+promedioAlumno = float(input("Ingrese el promedio del alumno: "))
+costoPension = float(input("Ingrese el costo de la pensión: "))
 
 # Verificamos si el promedio es mayor o igual a 9 para aplicar el descuento del 30%
-if promedio_Alumno >= 9:
-    descuento = 0.30 * costo_Pension
-    monto_Pagar = costo_Pension - descuento
+if promedioAlumno >= 9:
+    descuento = 0.30 * costoPension
+    montoPagar = costoPension - descuento
 else:
     # Si el promedio es menor que 9, el alumno debe pagar la pensión completa más el 10% de IVA
-    monto_Pagar = costo_Pension + 0.10 * costo_Pension
+    montoPagar = costoPension + 0.10 * costoPension
 
 # Imprime el monto a pagar
-print(f"El monto a pagar es: {monto_Pagar}")
+print(f"El monto a pagar es: {montoPagar}")
