7091e7e433428568b6f7a49645e878a5614578c9
Miguel070777 <160361356+Miguel070777@users.noreply.github.com>
2024-05-08 17:56:07 -0500

    ejercicio3Selectivas.py

--- estructurasSelectivas/ejercicio3Selectivas.py
+++ estructurasSelectivas/ejercicio3Selectivas.py
@@ -1,18 +1,18 @@
-cantidad_Total = float(input("ingrese el monto total de la compra:")) # Asignamos el monto total para empezar y como vamos a trabajar con decimales ponemos float
-if cantidad_Total_Total > 500000:
-    propio = cantidad_Total * 0.55
-    prestamo_Banco = monto_Total * 0.30
-    credito_Fabricante = monto_Total        # Ingresamos las variables y asignamos el porcentaje que nos indican en el ejercicio
+cantidadTotal = float(input("ingrese el monto total de la compra:")) # Asignamos el monto total para empezar y como vamos a trabajar con decimales ponemos float
+if cantidadTotal > 500000:
+    propio = cantidadTotal * 0.55
+    prestamoBanco = montoTotal * 0.30
+    creditoFabricante = montoTotal        # Ingresamos las variables y asignamos el porcentaje que nos indican en el ejercicio
 else:
-    propio = cantidad_Total * 0.70
-    credito_Fabricante = cantidad_Total - propio
+    propio = cantidadTotal * 0.70
+    credito_Fabricante = cantidadTotal - propio
     prestamo_Banco = 0                        # Si el monto no excede los 500.000 entonces entonces ingresamos el monto total por el porcentaje de la empresa
     
-interes_Credito_fabricante = credito_Fabricante * 0.20    # Asignamos una variable nueva que seria el interes del fabricante que seria el 20%
-total_Pago_credito_fabricante = credito_Fabricante + interes_Credito_fabricante
+interesCreditofabricante = creditoFabricante * 0.20    # Asignamos una variable nueva que seria el interes del fabricante que seria el 20%
+totalPagocreditofabricante = creditoFabricante + interesCreditofabricante
 print("Detalle del pago: ")
 print(f"Propio: COP{propio}")
-print(f"Prestamo del banco: COP{prestamo_Banco}")
-print(f"Interes por credito al fabricante: COP{interes_Credito_fabricante}")
-print(f"Total a pagar al fabricante: COP{total_Pago_credito_fabricante}")
+print(f"Prestamo del banco: COP{prestamoBanco}")
+print(f"Interes por credito al fabricante: COP{interesCreditofabricante}")
+print(f"Total a pagar al fabricante: COP{total_Pagocreditofabricante}")
 # Por ultimo solo queda imprimir para que nos indique el resultado de las variables 
