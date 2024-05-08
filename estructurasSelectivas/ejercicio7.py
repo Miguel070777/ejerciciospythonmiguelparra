93f7bda48167168a39e02fe61885fb9a2da3e16f
Miguel070777 <160361356+Miguel070777@users.noreply.github.com>
2024-05-08 17:59:05 -0500

    ejercicio7.py

--- estructurasSelectivas/ejercicio7.py
+++ estructurasSelectivas/ejercicio7.py
@@ -1,17 +1,17 @@
 #Le pedimos al usuario que digite la cantidad de llantas que desea comprar
-cantidad_Comprada = int(input("Ingrese la cantidad de llantas que desea comprar: ")) 
+cantidadComprada = int(input("Ingrese la cantidad de llantas que desea comprar: ")) 
 #Aca, definimos que si la cantidad de compra es igual , menor o mayor cobre segun el precio asignado
-if cantidad_Comprada < 5:
+if cantidadComprada < 5:
     precio = 300
-    total_A_pagar = cantidad_comprada * precio
-elif cantidad_Comprada<= 10:
+    totalApagar = cantidadcomprada * precio
+elif cantidadComprada<= 10:
     precio = 250
-    total_A_pagar = cantidad_Comprada * precio
-elif cantidad_Comprada > 10:
+    totalApagar = cantidadComprada * precio
+elif cantidadComprada > 10:
     precio = 200
-    total_A_pagar = cantidad_Comprada * precio
+    totalApagar = cantidadComprada * precio
 
 
 #Aca, es lo que se muestra segun los primeros datos y con la operacion pasada
 print(f"El costo por cada llanta es de ${precio}")
-print(f"El total a pagar por {cantidad_Comprada} llantas es de $ {total_A_pagar}")
+print(f"El total a pagar por {cantidadComprada} llantas es de $ {totalApagar}")
